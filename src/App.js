51fcf20dc69e6155ce25148606c8ebdcfcca09c7
verobega <pooh_tres@hotmail.com>
2020-07-15 20:32:10 -0500

    authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useFirebaseApp, useUser} from 'reactfire';
+import Auth from './Auth';
+
 
 function App() {
+  const user = useUser();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { user && <p>Usuario: {user.email} </p>}
+      <Auth />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import firebaseConfig from './firebase-config';
 
-ReactDOM.render(
+import{ FirebaseAppProvider } from 'reactfire'
+
+ReactDOM.render((
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
   <React.StrictMode>
+    <Suspense fallback={'Conectando con Sofi Clothes...'}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    </Suspense>
+  </React.StrictMode>
+  </FirebaseAppProvider>
+  ),document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
